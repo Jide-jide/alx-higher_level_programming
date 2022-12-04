d8d30ba43922fff743f23ad859b38de6302aa3cf
jide-jide <therealee90@gmail.com>
2022-12-04 13:10:04 +0000

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@
 
 def no_c(my_string):
 	 """Remove all characters c and C from a string."""
-	new_str = my_string.translate({ord(letter): None for letter in 'cC'})
-	return (new_str)
+	copy = [x for x in my_string if x != 'c' and x != 'C']
+	return ("".join(copy))
