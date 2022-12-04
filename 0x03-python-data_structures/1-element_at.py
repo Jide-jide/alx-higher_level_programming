6ebdb0d9fc166c8016cdd78863a3982763d1b4ed
jide-jide <therealee90@gmail.com>
2022-12-04 12:56:36 +0000

    task 1 fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 # 1-element_at.py
 
+
 def element_at(my_list, idx):
-	"""Retrieve an element from alist."""
-	n = len(my_list)
-	if idx < 0:
-		return None
-	elif idx >= n:
-		return None
-	else:
-		return (my_list[idx])
+    """Retrieve an element from alist."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
