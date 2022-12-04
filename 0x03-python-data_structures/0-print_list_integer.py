fbfe3cfbb7d20b45e050d1561bc2bfcf2810b051
jide-jide <therealee90@gmail.com>
2022-12-04 18:09:49 +0000

    fixed errors

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 def print_list_integer(my_list=[]):
     for x in my_list:
         print("{:d}".format(x))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-
-
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for element in matrix:
+        for number in element:
+            print("{:d}".format(number), end="")
+            if(number is not element[len(element)-1]):
+                print(" ", end="")
+        print()
