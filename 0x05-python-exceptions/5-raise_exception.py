fc12e21b20e7057723213dcc1692da8a0e8f8bd1
jide-jide <thereale90@gmail.com>
2022-12-19 19:12:46 +0000

    task 5 fix

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def raise_exception():
-	"""
-	function that raises a type exception.
-	
-	You are not allowed to import any module.
-	"""
-	raise TypeError
+    """function that raises a type exception.
+    You are not allowed to import any module.
+    """
+    raise TypeError
