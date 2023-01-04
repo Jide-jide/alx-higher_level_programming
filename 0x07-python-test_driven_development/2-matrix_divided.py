ed849d51653be4599c3484c41ee5f9fa9e4979b1
jide-jide <thereale90@gmail.com>
2023-01-04 16:41:31 +0000

    fixed indentaion errors

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,8 +6,9 @@ Attributes:
 """
 
 def matrix_divided(matrix, div):
+
     
-    """Divides all elements of a matrix.
+"""Divides all elements of a matrix.
     Args:
         matrix (list): A list of lists of integers or floats.
         div (int/float): Value to divide by.
@@ -20,22 +21,22 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: If div is equal to 0.
     Returns:
         matrix: A result of the division.
-    """
+"""
    
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if matrix == None or len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        
+
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-     
+
     matrix_len = len(matrix)
-    
+
     for i in range(matrix_len):
         if len(matrix[0]) == len(matrix[i]):
             pass
@@ -47,7 +48,7 @@ def matrix_divided(matrix, div):
                 pass
             else:
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        
+
     new_matrix = [[round(j / div, 2) for j in i] for i in matrix]
-    
+
     return new_matrix
