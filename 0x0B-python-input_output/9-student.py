00d3c97ca6b80e78eaa34e18549c2807d8a26e9a
jide-jide <thereale90@gmail.com>
2023-01-10 23:29:59 +0000

    requirment fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
-
-import json
+"""Module defining the class Student"""
 
 
 class Student:
-
-
+    """
+    Class that defines properties of student.
+    Attributes:
+        first_name (str): first name of student.
+        last_name (int): last name of student.
+        age (int): age of student.
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
