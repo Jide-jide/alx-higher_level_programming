c97a68cf0ca6de90273d05d2e37f59569f87abe3
jide-jide <thereale90@gmail.com>
2023-01-10 22:12:37 +0000

    code fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,9 +7,8 @@ and returns the number of characters written
 
 def write_file(filename="", text=""):
     """
-    writes a string to a text file (UTF8) 
-    and returns the number of characters written
+    writes a string to a text file (UTF8) and returns the number 
+    of characters written
     """
     with open(filename, "w", encoding="utf-8") as f:
-        data = f.write(text)
-        print(data)
+        return f.write(text)
