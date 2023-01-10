4446cdb0fccadda1377bb03f4b4b19316eb18456
jide-jide <thereale90@gmail.com>
2023-01-10 12:08:08 +0000

    added task 0 files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename) as f:
+        for l in f:
+            print(l)
